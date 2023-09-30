b7367622a453fd7b283e7fe4de55f48752b704df
echanjan <erickchanjan300696@.com>
2023-09-29 19:14:14 -0700

    update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,8 @@ def print_matrix_integer(matrix=[[]]):
             u = 1
             for o in i:
                 if u < len(i):
-                    print("{:d} ".format(o), end=' ')
-                else:
-                    print("{:d}".format(o))
+                    print("{:d} ".format(o), end='')
+                else:                                                                                                                     print("{:d}".format(o))
                 u += 1
     else:
-        print() 
+      print()
