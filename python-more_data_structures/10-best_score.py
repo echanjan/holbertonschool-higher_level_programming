932f16d85bfd4318dcdaeaaa3a1d6265efdc2c87
echanjan <erickchanjan300696@.com>
2023-10-04 06:18:18 -0700

    update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        best_value = max(a_dictionary.values(), default=None)
-        if best_value is not None:
-            return next(key for key, value in a_dictionary.items() if value == best_value)
+    if not a_dictionary:
+        return None
+    Max = 0
+    for key, value in a_dictionary.items():
+        if value > Max:
+            Max = value
+    for key, value in a_dictionary.items():
+        if value == Max:
+            return key
 
-    return None
