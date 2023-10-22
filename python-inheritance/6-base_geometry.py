181e766aa5587af320da96a0a4414aef4cebc44c
echanjan <erickchanjan300696@gmail.com>
2023-10-22 15:24:58 -0700

    update 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Este modulo definea una clase vacia.
+"""
+
+
+class BaseGeometry:
+    """
+    Esta clase define BaseGeomatry.
+    """
+    def area(self):
+        """
+        Esta funcion lanza una Exception.
+        """
+        raise Exception("area() is not implemented")
