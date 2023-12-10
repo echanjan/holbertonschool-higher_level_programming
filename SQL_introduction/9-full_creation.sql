1658de7cbe93ba3e83be0b6e3759b0c112fe8fad
echanjan <erickchanjan300696@gmail.com>
2023-12-10 14:48:51 -0800

    update 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,2 @@
+-- Crear unas segunda table en hbtn_0c_0 y agrega varias filas
+CREATE TABLE IF NOT EXISTS secont_table (id INT, name VARCHAR(256), score INT) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
