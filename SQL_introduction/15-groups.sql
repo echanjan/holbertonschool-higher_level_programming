e3d979bdbfbd2dc47215b7c1463b911c64e6659f
echanjan <erickchanjan300696@gmail.com>
2023-12-10 15:31:08 -0800

    update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Enumera la cantidad de registros en con mismo score en table second_table
-SELECT score, COUNT (score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
