eb29852c8caaf333aef868ed30463d3f9881a74a
echanjan <erickchanjan300696@gmail.com>
2023-12-10 20:18:37 -0800

    update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,4 +3,10 @@
 -- name VARCHAR(256) no puede ser null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL, FOREING KEY (state_id) REFERENCES states(states_id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS cities (
+	    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+	    state_id INT NOT NULL,
+	    name VARCHAR(256) NOT NULL,
+	    PRIMARY KEY (id),
+	    FOREIGN KEY (state_id) REFERENCES states(id)
+	);
