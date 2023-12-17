c2cf9a5b9d188318ecdd3c02ad98f204a2fc178e
echanjan <erickchanjan300696@gmail.com>
2023-12-17 15:35:47 -0800

    update 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
-sts all shows contained in the database hbtn_0d_tvshows.
+-- Lists all shows contained in the database hbtn_0d_tvshows.
 -- If a show doesn’t have a genre, display NULL
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT s.`title`, g.`genre_id`
