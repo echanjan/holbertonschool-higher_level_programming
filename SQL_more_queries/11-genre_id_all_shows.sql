774de1a1f57b68d6ec7bf9e9b2b317d54577f342
echanjan <erickchanjan300696@gmail.com>
2023-12-17 15:29:11 -0800

    update 11-genre_id_all_shows.sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+sts all shows contained in the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
