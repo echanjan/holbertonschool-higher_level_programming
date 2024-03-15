47393e8753404519a801e6d58220ff7d3f5fde55
echanjan <erickchanjan300696@gmail.com>
2024-03-15 18:56:20 -0500

    fix innerText

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,11 +1,11 @@
 const newCharacter = document.getElementById("character");
 
-fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+fetch("https://swapi-api.hbtn.io/api/people/4/?format=json")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    newCharacter.textContent = data.name;
+    newCharacter.innerText = data.name;
   })
   .catch((error) => {
     console.error("Error:", error);
